d3d624f9f2e38c0faa678ee91ad0c49498efe794
Doniyor27 <doni404error@gmail.com>
2021-08-23 16:09:28 +0500

    first

--- src/modules/getController.js
+++ src/modules/getController.js
@@ -57,44 +57,50 @@ const GET1 = (req, res) => {
     let products = fs.readFileSync(path.join(process.cwd(),'src','database','products.json'), 'UTF-8')
     let subCategoryJsonParse = JSON.parse(subCategory)
     let productsJsonParse = JSON.parse(products)
-    let { subcategoryId } = req.query
-    	let product = subCategoryJsonParse.filter( elem => elem.subCategoryId == subcategoryId )
-        for(let i of product) {
-            let arr = []
-            for(let g of productsJsonParse) {
-                if(i.subCategoryId == g.subCategoryId) {
-                    let obj = {
-                        productId: g.productId,
-                        productName: g.productName,
-                        model: g.model,
-                        price: g.price,
-                        color: g.color
+    let search = req.query
+    function filter(arr, search) {
+        let storage = []
+            for (let i in arr) {
+                let counter = 0
+                let length = 0
+                for (let j in search) {
+                    if (search[j] == arr[i][j]) {
+                        counter++
                     }
-                    arr.push(obj)
+                    length += 1
+                }
+                if (counter === length) {   
+                    storage.push(arr[i]);
                 }
             }
-            i.products = arr
-        }
-    	return res.json( product || { message: 'not found!' } )
+    
+            return storage
+    
+    }
+
+    let response = filter(productsJsonParse,search)
+    	// let product = subCategoryJsonParse.filter( elem => elem.subCategoryId == subcategoryId )
+        // for(let i of product) {
+        //     let arr = []
+        //     for(let g of productsJsonParse) {
+        //         if(i.subCategoryId == g.subCategoryId) {
+        //             let obj = {
+        //                 productId: g.productId,
+        //                 productName: g.productName,
+        //                 model: g.model,
+        //                 price: g.price,
+        //                 color: g.color
+        //             }
+        //             arr.push(obj)
+        //         }
+        //     }
+        //     i.products = arr
+        // }
+    	return res.json( response || { message: 'not found!' } )
 
     // res.send(subCategoryJsonParse)
 }
 
-
-
-
-
-// app.get('/posts/:post_id', (req, res) => {
-// 	let { post_id } = req.params
-// 	let post = postJsonParse.find( elem => elem.post_id == post_id )
-// 	return res.json( post || { message: 'not found!' } )
-// })
-
-// app.get('/posts/users/:user_id', (req, res) => {
-// 	let { user_id } = req.params
-// 	let posts = postJsonParse.find( elem => elem.user_id == user_id )
-// 	return res.json( posts || { message: 'not found!' } )
-// } )
 
 
 export {
